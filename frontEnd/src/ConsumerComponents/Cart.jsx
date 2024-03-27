cb42322b61119230cdad72977777407c82a108a1
pnkkumar123 <pankajkanwar420@gmail.com>
2024-03-27 10:25:45 +0530

    added css effects cart and consumer products page

--- frontEnd/src/ConsumerComponents/Cart.jsx
+++ frontEnd/src/ConsumerComponents/Cart.jsx
@@ -1,29 +1,64 @@
 import {useSelector} from 'react-redux'
-import { Typography, List, ListItem, ListItemText } from '@mui/material';
+import styled,{keyframes} from 'styled-components';
 import { useGetCartQuery } from '../redux/slice/ProductSlice';
 
 function Cart() {
  const id = useSelector((state)=>state?.user?.currentUser?.user?._id)
   const { data, isFetching, error } = useGetCartQuery(id);
 console.log(data?.cart?.items);
-  if (isFetching) return <div>Loading...</div>;
+  if (isFetching) return <LoadingSpinner/>;
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div style={{ maxWidth: 600, margin: 'auto' }}>
+    <CartContainer>
      {data?.cart?.items && data?.cart?.items.map((item)=>{
       const {productName,price,photo,_id} = item
       return(
-        <div key={_id}>
+        <CartItem key={_id}>
+          <ProductImage src={photo} alt="" />
+          <div>
           <p>{productName}</p>
-          <img src={photo} alt="" />
+          <p>Price:${price}</p>
+          </div>
 
-        </div>
+        </CartItem>
       )
      })}
-    </div>
+    </CartContainer>
   );
 }
+const loadingAnimation = keyframes`
+0%{
+  transform:rotate(0deg);
+}
+100%{
+  transform:rotate(360deg);
+}
+`;
+const CartContainer = styled.div`
+max-width:600px;
+margin:auto;'
 
+`
+const CartItem = styled.div`
+display:flex;
+align-items:center;
+margin-bottom:20px;
+`
+const ProductImage = styled.img`
+width:100px;
+height:100px;
+object-fit:cover;
+margin-right:20px;
+`
+const LoadingSpinner = styled.div`
+border:4px solid #f3f3f3;
+border-top:4px solid #3498db;
+border-radius:50%;
+width:30px;
+height:30px;
+animation:${loadingAnimation} 1s linear infinite;
+margin:auto;
+`
 export default Cart;
 
--- frontEnd/src/ConsumerComponents/ConsumerProducts.jsx
+++ frontEnd/src/ConsumerComponents/ConsumerProducts.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useGetProductQuery } from '../redux/slice/ProductSlice';
-import { Grid, Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
-import {NavLink, useNavigate } from 'react-router-dom';
-
-
+import { Grid, Typography, Button } from '@mui/material';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToCart } from '../redux/slice/CartSlice';
 
+
+// Styled components
+
+// Component
 function ConsumerProducts() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const currentUser = useSelector((state) => state.user.currentUser?.user?._id);
   const dispatch = useDispatch();
   const { data, isFetching, error } = useGetProductQuery();
-
+  
   const handleAddToCart = async (productId, productName, price, photo) => {
     try {
-      if(!currentUser){
-       navigate('/consumersignin')
+      if (!currentUser) {
+        navigate('/consumersignin');
       }
       const userId = currentUser;
       dispatch(addToCart(productId));
@@ -36,56 +39,73 @@ function ConsumerProducts() {
       console.log(e);
     }
   };
-
+  
   if (isFetching) return <p>Loading...</p>;
   if (error) return <p>Error..</p>;
   if (!data || !data.products || data.products.length === 0) return <p>No products found</p>;
-
+  
   return (
-    <div className='products'>
-      <h1>Products</h1>
-      <Grid container spacing={2} alignItems="stretch">
-        {data.products.map((product) => {
-          const { _id, photo, description, productName, price, category, brand, quantityAvailable } = product;
-          return (
-            <Grid item xs={12} sm={6} md={3} key={_id}>
-              <Card>
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image={photo ? photo : "https://img.freepik.com/free-vector/flat-design-no-data-illustration_23-2150527130.jpg?w=740&t=st=1710335455~exp=1710336055~hmac=856ef2a2e754625212837ec6499c88f1a50e2bad53016ba349817b5fa34f1af5"}
-                  alt={productName}
-                />
-                <CardContent>
-                  <Typography variant='h6' component="div" >
-                    {productName}
-                  </Typography>
-                  <Typography variant='subtitle1' color="text.secondary">
-                    Price: ${price}
-                  </Typography>
-                  <Typography variant='body2' color="text.secondary">
-                    Description: {description ? (description.length > 90 ? `${description.slice(0, 50)}...` : description) : 'No description available'}
-                  </Typography>
-                  <Typography variant='body2' color="text.secondary">
-                    Category: {category}
-                  </Typography>
-                  <Typography variant='body2' color="text.secondary">
-                    Brand: {brand}
-                  </Typography>
-                  <Typography variant='body2' color="text.secondary">
-                    Quantity Available: {quantityAvailable}
-                  </Typography>
-                  {currentUser ? (<Button variant='contained' color='primary' onClick={() => handleAddToCart(_id, productName, price, photo)} >Add to Cart</Button> ): (<Button variant='contained' color='primary' ><NavLink to='/consumersignin'>Add to Cart</NavLink> </Button>)}
-                </CardContent>
-              </Card>
+    <ProductsContainer>
+      <Sidebar>
+       
+      </Sidebar>
+      <Content>
+        <Typography variant="h1">Products</Typography>
+        <Grid container spacing={2}>
+          {data.products.map((product) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={product._id}>
+              <ProductCard>
+                <ProductImage src={product.photo || "https://img.freepik.com/free-vector/flat-design-no-data-illustration_23-2150527130.jpg?w=740&t=st=1710335455~exp=1710336055~hmac=856ef2a2e754625212837ec6499c88f1a50e2bad53016ba349817b5fa34f1af5"} alt={product.productName} />
+                <Typography variant='h6'>{product.productName}</Typography>
+                <Typography variant='subtitle1'>Price: ${product.price}</Typography>
+                {/* <Typography variant='body2'>Description: {product.description ? (product.description.length > 90 ? `${product.description.slice(0, 50)}...` : product.description) : 'No description available'}</Typography> */}
+                <Typography variant='body2'>Category: {product.category}</Typography>
+                <Typography variant='body2'>Brand: {product.brand}</Typography>
+                <Typography variant='body2'>Quantity Available: {product.quantityAvailable}</Typography>
+                {currentUser ? (
+                  <Button variant='contained' color='primary' onClick={() => handleAddToCart(product._id, product.productName, product.price, product.photo)}>Add to Cart</Button>
+                ) : (
+                  <Button variant='contained' color='primary'>
+                    <NavLink to='/consumersignin'>Add to Cart</NavLink>
+                  </Button>
+                )}
+              </ProductCard>
             </Grid>
-          );
-        })}
-      </Grid>
-    </div>
+          ))}
+        </Grid>
+      </Content>
+    </ProductsContainer>
   );
 }
+const ProductsContainer = styled.div`
+  display: flex;
+`;
+
+const Sidebar = styled.div`
+  width: 250px;
+  padding: 20px;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ProductCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+const ProductImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 5px;
+`;
 
 export default ConsumerProducts;
-
 
