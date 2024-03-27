141b6a79b8f4b25fe659e339502939f8493c9dc2
pnkkumar123 <pankajkanwar420@gmail.com>
2024-03-27 09:31:05 +0530

    added css effects to seller signup page

--- frontEnd/src/AdminComponents/SignUp.jsx
+++ frontEnd/src/AdminComponents/SignUp.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import styled from 'styled-components';
+
 
 function SignUp() {
   const [loading, setLoading] = useState(false);
@@ -41,17 +43,19 @@ function SignUp() {
   };
 
   return (
-    <div>
+    <SignUpWrapper>
+      <SignUpContainer>
       {error && <p>Error: Failed to sign up. Please try again later.</p>}
-      <form onSubmit={handleSubmit}>
-        <input
+      <SignUpForm onSubmit={handleSubmit}>
+        <SignUpTitle>Seller Sign Up</SignUpTitle>
+        <SignUpInput
           onChange={handleChange}
           type="text"
           name="name"
           placeholder="Name"
           id="name"
         />
-        <input
+        <SignUpInput
           onChange={handleChange}
           type="text"
           name="userName"
@@ -59,24 +63,78 @@ function SignUp() {
           placeholder="UserName"
         />
 
-        <input
+        <SignUpInput
           onChange={handleChange}
           type="email"
           name="email"
           placeholder="Email"
           id="email"
         />
-        <input
+        <SignUpInput
           onChange={handleChange}
           type="password"
           name="password"
           placeholder="Password"
           id="password"
         />
-        <button disabled={loading}>{loading ? "Loading..." : "Sign Up"}</button>
-      </form>
-    </div>
+        <SignUpButton disabled={loading}>{loading ? "Loading..." : "Sign Up"}</SignUpButton>
+      </SignUpForm>
+      </SignUpContainer>
+    </SignUpWrapper>
   );
 }
+const SignUpWrapper = styled.div`
+background-size:cover;
+background-position:center;
+min-height:80vh;
+display:flex;
+margin:auto;
+justify-items:center;
+align-items:center;
 
+`;
+const SignUpContainer = styled.div`
+background-color:rgba(170,190,244,0.7);
+padding:20px;
+border-radius:8px;
+box-shadow:0 0 10px rgba(111,222,177,0.3);
+widht:600px;
+display:flex;
+flex-direction:column;
+align-items:center;
+`;
+
+const SignUpForm = styled.form`
+display:flex;
+flex-direction:column;
+gap:20px;
+`
+const SignUpTitle = styled.h1`
+font-size:24px;
+text-align:center;
+
+`
+const ErrorMessage = styled.p`
+color:red;
+`;
+const SignUpInput = styled.input`
+padding:10px;
+box-shadow:0 0 10px rgba (111,222,177,0.3);
+border:1px solid #ccc;
+border-radius:4px;
+`
+const SignUpButton = styled.button`
+padding:10px;
+background-color:#007bff;
+color:#fff;
+border:none;
+border-radius:4px;
+cursor:pointer;
+
+&:disabled{
+  opacity:0.8;
+  cursor:not-allowed;
+
+}
+`
 export default SignUp;
