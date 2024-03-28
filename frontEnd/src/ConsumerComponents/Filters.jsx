b837e09161904fc64bc52e88801dce551715ecd6
pnkkumar123 <pankajkanwar420@gmail.com>
2024-03-28 11:41:51 +0530

    applied css effects to my filters

--- frontEnd/src/ConsumerComponents/Filters.jsx
+++ frontEnd/src/ConsumerComponents/Filters.jsx
@@ -6,28 +6,40 @@ function Filters({ categories, onCategoryClick, onClearFilters }) {
     <div>
      
       <CategoryList>
+        <Title>Category</Title>
         {categories.map((category, index) => (
-          <Button key={index} onClick={() => onCategoryClick(category)}>{category}</Button>
+          <CategoryButton  key={index} onClick={() => onCategoryClick(category)}>{category}</CategoryButton>
         ))}
       </CategoryList>
       <Button style={{padding:12,borderRadius:5}} onClick={() => onClearFilters()}>Clear Filters</Button>
     </div>
   );
 }
-
-const Button = styled.button`
-  margin-right: 10px;
-  margin:20px;
-  align-items:center;
-  jutify-items:center;
-  
+const Title = styled.h5`
+  font-size: bold;
+  text-align: center; /* Center the text horizontally */
+  margin-bottom: 10px; /* Add margin below the title */
 `;
 
-
+const CategoryButton = styled.button`
+  margin-right: 10px;
+  padding: 10px;
+  margin-bottom: 10px; /* Add margin between buttons */
+  width: 150px; /* Set a fixed width for the buttons */
+`;
+const Button = styled.button`
+margin-right: 10px;
+  margin-bottom: 10px;
+`
 const CategoryList = styled.div`
-box-shadow: 0 0 10px rgba(111, 222, 177, 0.3);
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Center the items horizontally */
+  box-shadow: 0 0 10px rgba(111, 222, 177, 0.3);
+  padding: 20px;
   margin-bottom: 20px; /* Add margin to create gap between Clear Filters and category buttons */
 `;
+
 
 export default Filters;
 
