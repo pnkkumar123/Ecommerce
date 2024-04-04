849af8a4c67426212bfcc574ab76cbe787da63af
pnkkumar123 <pankajkanwar420@gmail.com>
2024-04-04 13:36:33 +0530

    styling of cart componet is completed

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -34,27 +34,11 @@ import { ThemeProvider } from 'styled-components'
 function App() {
   const {currentUser} = useSelector((state)=>state.user)
   const isSeller = useSelector((state)=>state.user.isSeller)
-  const theme = {
-    colors: {
-      bg: "#F6F8FA",
-      footer_bg: "#0a1435",
-      btn: "rgb(98 84 243)",
-      border: "rgba(98,84,243,0.5)",
-      hr: "#fff",
-      gradient: "linear-gradient(0deg,rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
-      shadow: 
-      "rgba(0,0,0,0.02) 0px 1px 3px 0px,rgba(27,31,35,0.16)0px 0px 0px 1px",
-      shadowSupport: "rgba(0,0,0,0.16)0px 1px 4px",
-    },
-    media: {
-      mobile: "768px",
-      tab: "998px",
-    },
-  }
+ 
   return (
     <Wrapper>
-      <ThemeProvider theme={theme}>
-      <GlobalStyle/>
+      
+   
   <div className='navabar'>
    <Navbar/>
   
@@ -91,7 +75,7 @@ function App() {
       </Route>
     </Routes>
    </div>
-      </ThemeProvider>
+
     </Wrapper>
   )
 }
