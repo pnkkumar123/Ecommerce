1b03336bb139a494ec1cce24d7db11c1b271c650
pnkkumar123 <pankajkanwar420@gmail.com>
2024-04-06 09:57:16 +0530

    pre Build

--- backEnd/index.js
+++ backEnd/index.js
@@ -33,10 +33,10 @@ res.status(200).json({key:process.env.RAZORPAY_API_KEY})
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/frontEnd/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontEnd", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
