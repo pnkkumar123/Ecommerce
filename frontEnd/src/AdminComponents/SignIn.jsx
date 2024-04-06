0dae40208b6953221c36ea6b90d8351fb6bb2c0e
pnkkumar123 <pankajkanwar420@gmail.com>
2024-04-06 15:29:41 +0530

    did some proxy things.

--- frontEnd/src/AdminComponents/SignIn.jsx
+++ frontEnd/src/AdminComponents/SignIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {toast} from 'react-toastify'
+
 import styled from 'styled-components';
 import {
   SellerSignInFailure,
--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-   
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        secure: false,
+      },
+    },
   },
 
   plugins: [react()],
